78d3ffb3488e6d02634ba3e04bcec6dacf7e3260
AndrewNemnojko <106536698+AndrewNemnojko@users.noreply.github.com>
2025-10-08 23:18:13 +0300

    v.5 (upd: update core, create awards-list carousel component)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
-
+import { provideRouter} from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -10,6 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch())
   ],
 };
