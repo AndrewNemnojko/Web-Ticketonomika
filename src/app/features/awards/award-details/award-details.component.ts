3326af9c26176a931aef900ba68ef0b483583d15
AndrewNemnojko <106536698+AndrewNemnojko@users.noreply.github.com>
2025-10-09 22:06:00 +0300

    v.46 (upd: add shared popup, add award details)

---
+++ src/app/features/awards/award-details/award-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Award } from '../../../shared/models/award.model';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-award-details',
+  imports: [DatePipe],
+  templateUrl: './award-details.component.html',
+  styleUrl: './award-details.component.scss'
+})
+export class AwardDetailsComponent {
+  @Input() award: Award | null = null;
+}
